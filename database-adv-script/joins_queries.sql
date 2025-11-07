c3a8dd4d66429ab7c133a56aa47ce86c66c372f0
Gideon Oba <deyonoba@gmail.com>
2025-11-07 08:53:17 +0100

    Add 'joins_queries.sql' -> 'database-adv-script'

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,32 @@
+USE airbnb_clone_db;
+
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.created_at AS booking_created_at,
+    b.property_id,
+    b.total_price,
+    b.status
+FROM User AS u
+INNER JOIN Booking AS b 
+    ON u.user_id = b.user_id;
+
+
+SELECT
+    p.property_id,
+    p.host_id,
+    p.name AS property_name,
+    p.location,
+    p.price_per_night,
+    r.review_id,
+    r.rating,
+    r.comment
+FROM Property AS p
+JOIN Review AS r
+    ON p.property_id = r.property_id;
+
+SELECT * FROM User FULL OUTER JOIN Booking ON User.user_id = Booking.user_id;
