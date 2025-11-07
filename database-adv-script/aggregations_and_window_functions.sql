3b20d48257cbcbcef8ebf6e4f4ec4c571236a9f4
Gideon Oba <deyonoba@gmail.com>
2025-11-07 16:55:41 +0100

    Add 'aggregations_and_window_functions.sql' -> 'database-adv-script'

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,19 @@
+USE airbnb_clone_db;
+
+-- Query to get the total number of bookings made by each user
+SELECT u.user_id, COUNT(b.user_id) as count
+FROM User u
+LEFT JOIN Booking b
+    ON u.user_id = b.user_id
+GROUP BY u.user_id;
+
+
+SELECT
+    p.property_id,
+    COUNT(b.property_id) AS total_booking,
+    RANK() OVER (ORDER BY COUNT(b.property_id) DESC) AS booking_rank,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.property_id) DESC) AS booking_row_number
+FROM Property AS p
+JOIN Booking AS b
+    ON p.property_id = b.property_id
+GROUP BY p.property_id;
